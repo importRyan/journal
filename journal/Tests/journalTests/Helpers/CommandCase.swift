34ebf1d40baf4104ed35e3c532f93f3951d5fa61
importRyan <importRyan@gmail.com>
2021-09-29 19:25:50 -0700

    Add XCTestCase command test case convenience

---
+++ journal/Tests/journalTests/Helpers/CommandCase.swift
@@ -0,0 +1,45 @@
+//  © 2021 Ryan Ferrell. github.com/importRyan
+
+import Foundation
+import ArgumentParser
+import XCTest
+
+extension XCTestCase {
+    static let appName = "journal"
+}
+
+struct CommandCase {
+
+    let command: String
+    let expectedOutput: String
+    let exitCode: ExitCode
+
+    func userInput() -> String {
+        XCTestCase.appName + " " + command
+    }
+
+    init(command: String, expectedOutput: String, exitCode: ExitCode) {
+        self.command = command
+        self.expectedOutput = expectedOutput
+        self.exitCode = exitCode
+    }
+
+    init(valid: String, expectedOutput: String) {
+        self.command = valid
+        self.expectedOutput = expectedOutput
+        self.exitCode = .success
+    }
+
+    init(incomplete: String, expectedOutput: String) {
+        self.command = incomplete
+        self.expectedOutput = expectedOutput
+        self.exitCode = .validationFailure
+    }
+
+    init(failing: String, expectedOutput: String) {
+        self.command = failing
+        self.expectedOutput = expectedOutput
+        self.exitCode = .failure
+    }
+}
+
