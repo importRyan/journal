2f5c7d60e921949b4906f6c46e7f5d7c0ff0992c
importRyan <importRyan@gmail.com>
2021-09-29 20:49:51 -0700

    Launch sequence for command line app

--- journal/Sources/journal/main.swift
+++ journal/Sources/journal/main.swift
@@ -1,1 +1,39 @@
-print("Hello, world!")
+//  © 2021 Ryan Ferrell. github.com/importRyan
+
+import Foundation
+import Journaling
+import ArgumentParser
+
+// App lazily loaded after parsing command line input.
+fileprivate(set) var app: JournalingApp! = nil
+fileprivate let loader = MockLoader()
+
+// Launch a command line user interface.
+OptionsOnlyInterface.main()
+
+// Hold loop open for any async background tasks (e.g., persistence).
+RunLoop.main.run()
+
+
+// MARK: - Helper Methods
+
+func startApp(config: AppConfig = DevelopmentConfig(), didStart: @escaping () -> Void) {
+    guard app == nil else { reportLoadAttempt(); return }
+    do {
+        app = try JournalApp(loader: loader, config: config)
+        app.start { error in
+            if let error = error { exit(with: error) }
+            else { didStart() }
+        }
+    } catch { exit(with: error) }
+}
+
+fileprivate func reportLoadAttempt() {
+    app.logger.log(event: "Attempted to load app more than once.")
+}
+
+fileprivate func exit(with error: Error) {
+    NSLog("Journal app failed to load.")
+    NSLog(error.localizedDescription)
+    exit(EXIT_FAILURE)
+}
