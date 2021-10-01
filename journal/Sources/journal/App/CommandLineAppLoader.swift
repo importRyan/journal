a87f5b0ee3e6a3f4e79284df8e3dd8fe7b2a7891
importRyan <importRyan@gmail.com>
2021-09-30 19:13:21 -0700

    Add persistence to journal with an AppLoader implementation

---
+++ journal/Sources/journal/App/CommandLineAppLoader.swift
@@ -0,0 +1,37 @@
+//  © 2021 Ryan Ferrell. github.com/importRyan
+
+
+import Foundation
+import Journaling
+import JournalingPersistence
+
+internal class CommandLineLoader: AppLoader {
+
+    init() { }
+
+    func load(with configuration: AppConfig) -> Result<AppLoadables, Error> {
+
+        let logger = SystemLogger(label: "commandline")
+
+        let persistence = LocalPersistenceManager(
+            mode: configuration.loadEntries,
+            location: configuration.journalLocation,
+            logger: logger
+        )
+
+        let store = MockJournalStore(
+            persistence: persistence,
+            logger: logger
+        )
+
+        let formatter = JJStaticFormattingStore(formatter: configuration.formatting)
+
+        return .success(
+            .init(persistence: persistence,
+                  store: store,
+                  logger: logger,
+                  formatting: formatter)
+        )
+    }
+
+}
