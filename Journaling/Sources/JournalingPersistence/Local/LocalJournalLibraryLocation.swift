7658bc0741f3c313d1245a60bfb492d21d650281
importRyan <importRyan@gmail.com>
2021-09-30 18:33:05 -0700

    Extend JournalLibraryLocation to find relevant local folder

---
+++ Journaling/Sources/JournalingPersistence/Local/LocalJournalLibraryLocation.swift
@@ -0,0 +1,41 @@
+//  © 2021 Ryan Ferrell. github.com/importRyan
+
+
+import Foundation
+import Journaling
+
+internal extension JournalLibraryLocation {
+
+    static let localAppDirectoryName = "Journal"
+    static let librarySubfolderName = "UserData"
+
+    func getAppUserDataFolder() throws -> URL {
+        let fm = FileManager.default
+        guard let url = self.localURLs().first else {
+            throw LocalPersistenceError.chosenDirectoryNotReachable
+        }
+
+        let appDirectoryName = Self.localAppDirectoryName
+        let libraryDirectoryName = Self.librarySubfolderName
+        let libraryURL = url
+            .appendingPathComponent(appDirectoryName, isDirectory: true)
+            .appendingPathComponent(libraryDirectoryName, isDirectory: true)
+
+        var pathIsDirectory = ObjCBool(false)
+        if !fm.fileExists(atPath: libraryURL.path, isDirectory: &pathIsDirectory) {
+            try fm.createDirectory(at: libraryURL, withIntermediateDirectories: true, attributes: nil)
+        }
+
+        return libraryURL
+    }
+
+    func localURLs() -> [URL] {
+        FileManager.default.urls(for: self.localSearchPath, in: .userDomainMask)
+    }
+
+    var localSearchPath: FileManager.SearchPathDirectory {
+        switch self {
+            case .desktop: return .desktopDirectory
+        }
+    }
+}
